f5ea8199c9d3d2f356cbb009f716bed54c4acf7c
zhangdelong <zhangdelong@dongdao.net>
2015-12-28 18:36:47 +0800

    格式化日志输出，增加文件大小正常人方式显示

--- backup_mongo.py
+++ backup_mongo.py
@@ -6,32 +6,32 @@ import sys
 from os.path import join
 
 reload(sys)
-print '%s  : 系统当前默认字符集是%s' % (datetime.datetime.now(), sys.getdefaultencoding())
+print '---------------------------------------------------------------------\n'
+print '%s   : ........正在进行备份请稍后..........\n'%datetime.datetime.now()
+print '%s   : 系统当前默认字符集是： %s\n' % (datetime.datetime.now(), sys.getdefaultencoding())
 sys.setdefaultencoding('utf-8')
-print '%s  : 修改当前系统字符集为%s' % (datetime.datetime.now(), sys.getdefaultencoding())
+print '%s   : 修改当前系统字符集为： %s\n' % (datetime.datetime.now(), sys.getdefaultencoding())
 # 待导出数据库信息
 db_user = 'admin'
 db_passwd = 'admin'
 db_type = 'mongodb'
 mongo_shell_path = '/usr/local/mongodb/bin/mongodump'
 mongo_shell_parameter = ' --authenticationDatabase admin'
-print '%s  : 待备份数据库类型 %s' % (datetime.datetime.now(), db_type)
-#备份文件命名。
+print '%s   : 待备份数据库类型是： %s\n' % (datetime.datetime.now(), db_type)
+#备份文件命名
 TARGET_DIR = '/data/backup/'
-print '%s   : 备份到本地 %s目录下 ' % (datetime.datetime.now(), TARGET_DIR)
-mongo_file_path = TARGET_DIR+db_type + time.strftime('_%H%M%S')
-print '%s  : 数据库导出文件名是 %s' % (datetime.datetime.now(), mongo_file_path)
-TARGET =TARGET_DIR+ 'mongoDB' + time.strftime('%Y%m%d') + '.tar'
-print '%s  : 压缩包文件名是 %s' % (datetime.datetime.now(), TARGET)
+print '%s   : 备份到本地 %s目录下\n ' % (datetime.datetime.now(), TARGET_DIR)
+mongo_file_path = TARGET_DIR+db_type + time.strftime('-%H%M%S')
+print '%s   : 数据库导出文件夹是： %s\n' % (datetime.datetime.now(), mongo_file_path)
+TARGET =TARGET_DIR+ 'mongoDB-' + time.strftime('%Y%m%d') + '.tar'
+print '%s   : 压缩包文件名是： %s\n' % (datetime.datetime.now(), TARGET)
 #备份数据库命令
 bak_mongo_shell = mongo_shell_path + ' -o ' + mongo_file_path + ' -u ' + db_user + ' -p=' + db_passwd + mongo_shell_parameter
-print '%s  : 备份数据库命令是 %s' % (datetime.datetime.now(), bak_mongo_shell)
+#数据库备份命令原型：/usr/local/mongodb/bin/mongodump -o /data/backup/mongodb-181746 -u admin -p=admin --authenticationDatabase admin
 #备份压缩命令
 tar_command = 'tar -czf %s %s ' % (TARGET, ''.join(mongo_file_path))
-print '%s  : 备份压缩命令是 %s' % (datetime.datetime.now(), tar_command)
+print '%s   : 备份压缩命令是： %s\n' % (datetime.datetime.now(), tar_command)
 #执行备份数据库命令
-print '-------------------------------------------------'
-print '................备份中............................'
 print
 os.system(bak_mongo_shell)
 # 判断导出的mongo文件夹大小，来确定是否执行打包命令
@@ -46,15 +46,29 @@ def getdirsize(dir):
     return size
 
 mongo_file_size = getdirsize(mongo_file_path)
-print '%s  : 当前导出的mongodb文件夹大小是 %s' % (datetime.datetime.now(), mongo_file_size)
+# 统计导出的备份文件大小
+def format_file(format_file_size):
+    size1=format_file_size/(1024.0*1024.0*1024.0)#GB
+    size2=format_file_size/(1024.0*1024.0)#MB
+    size3=format_file_size/1024.0#KB
+    size4=format_file_size#B
+    if      size1>1:
+        print "%s   : 当前备份的文件大小是： %sGB\n"%(datetime.datetime.now(),round(size1,1))
+    elif    size2>1:
+        print "%s   : 当前备份的文件大小是： %sMB\n"%(datetime.datetime.now(),round(size2,1))
+    elif    size3>1:
+        print "%s   : 当前备份的文件大小是： %sKB\n"%(datetime.datetime.now(),round(size3,1))
+    else:#这里对文件大小进行判断，当文件大于1M显示的是多少MB，如果当文件小于1M显示的是多少KB，利用round函数进行四舍五入
+        print "%s   : 当前备份的文件大小是： %sB\n"%(datetime.datetime.now(),round(size4,3))
 while True:
     if mongo_file_size == mongo_file_size:
         #如果文件大小相同说明导出到文件已经结束。执行打包命令
-        print '%s  : 正在执行压缩命令 %s' % (datetime.datetime.now(), tar_command)
         os.system(tar_command)
         tar_file_size = os.path.getsize(TARGET)
-        print '%s  : 当前导出的压缩包大小是 %s' % (datetime.datetime.now(), tar_file_size)
+        format_file(tar_file_size)
+        print '%s   : ........备份成功！！..........\n' %datetime.datetime.now()
+        print '%s   : 备份文件为： %s\n' % (datetime.datetime.now(), TARGET)
         break
 print
-print '...............备份结束...........................'
-print '-------------------------------------------------'
+print '---------------------------------------------------------------------\n'
+
