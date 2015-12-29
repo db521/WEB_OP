364ccd4712f5e0a967163876704f4d602e6e41b5
zhangdelong <zhangdelong@dongdao.net>
2015-12-29 12:44:46 +0800

    修改本地备份的目录为/data/backup/201512/这种。

--- backup_folder.py
+++ backup_folder.py
@@ -8,13 +8,11 @@ print '---------------------------------------------------------------------\n'
 print '%s   : ........正在进行备份请稍后..........\n'%datetime.datetime.now()
 SOURCE = ['/deploy/']
 print '%s   : 即将备份的目录是： %s\n'% (datetime.datetime.now(), SOURCE)
-TARGET_DIR = '/data/backup/'
+TARGET_DIR = '/data/backup/'+ time.strftime('%Y%m') + "/"#本地备份到/data/backup/201512/目录下
 print '%s   : 即将备份到 %s 目录下\n' % (datetime.datetime.now(), TARGET_DIR)
 NAME_FILE = 'deploy' + time.strftime('-%H%M%S')
 print '%s   : 即将生成的备份文件名是： %s\n' % (datetime.datetime.now(), NAME_FILE)
-today = TARGET_DIR+time.strftime('%Y%m%d')
-print '%s   : 即将生成当前系统日期的目录是： %s\n' % (datetime.datetime.now(), today)
-TARGET = TARGET_DIR + time.strftime('%Y%m%d') + "/" + NAME_FILE + '.tar'
+TARGET = TARGET_DIR + NAME_FILE + '.tar'
 tar_command = 'tar -cvzf %s %s ' % (TARGET, ' '.join(SOURCE))
 print '%s   : 压缩命令是： %s\n' % (datetime.datetime.now(), tar_command)
 #判断/data/backup/是否存在
@@ -29,9 +27,6 @@ def mkdir_p(TARGET_DIR):
         else:
             raise
 mkdir_p(TARGET_DIR)
-if not os.path.exists(today):#判断当前日期的文件夹是否存在
-    os.mkdir(today)  # 创建目录
-    print '%s   : 当前日期的目录创建成功： %s\n' % (datetime.datetime.now(), today)
 #执行压缩命令
 if os.system(tar_command) ==0:
     print '%s   : ........备份成功！！..........\n' %datetime.datetime.now()
