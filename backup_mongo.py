b50805c8d385c0e051c696700696fb1577818a37
zhangdelong <zhangdelong@dongdao.net>
2016-01-05 17:42:46 +0800

    1、修改mongo备份脚本关于mongo导出部分的代码，改成为如果当前导出出错，给出系统返回错误码。 2、增加判断，如果mongo导出失败，不进行后面的压缩备份。 3、修改tar备份部分代码，改成如果当前tar命令执行失败，给出系统返回错误码。 4、增加备份时长统计。

--- backup_mongo.py
+++ backup_mongo.py
@@ -2,11 +2,12 @@
 # -*- coding:utf-8 -*-
 
 import os, time, datetime
-import sys
+import sys,subprocess
 from os.path import join
 
 reload(sys)
 print '---------------------------------------------------------------------\n'
+time1=datetime.datetime.now()#增加统计时长计算，和脚本最后面进行相减操作，计算出脚本执行时长
 print '%s   : ........正在进行备份请稍后..........\n'%datetime.datetime.now()
 print '%s   : 系统当前默认字符集是： %s\n' % (datetime.datetime.now(), sys.getdefaultencoding())
 sys.setdefaultencoding('utf-8')
@@ -33,7 +34,20 @@ tar_command = 'tar -czf %s %s ' % (TARGET, ''.join(mongo_file_path))
 print '%s   : 备份压缩命令是： %s\n' % (datetime.datetime.now(), tar_command)
 #执行备份数据库命令
 print
-os.system(bak_mongo_shell)
+#os.system(bak_mongo_shell)
+try:#判断mongo导出是否成功
+    retcode =subprocess.call(bak_mongo_shell, shell=True)
+    if retcode < 0:
+        print >>sys.stderr, "mongo导出子进程被终止，返回码是", -retcode
+        print '%s   : ........mongo导出备份失败！！..........\n' %datetime.datetime.now()
+    if retcode ==0:
+        print '%s   : ........mongo导出成功！！..........\n' %datetime.datetime.now()
+    else:
+        print >>sys.stderr, "mongo导出子进程返回码是", retcode
+        print '%s   : ........mongo导出备份失败！！..........\n' %datetime.datetime.now()
+except OSError as e:
+    print >>sys.stderr, "mongo导出错误信息:", e
+    print '%s   : ........mongo导出备份失败！！..........\n' %datetime.datetime.now()
 # 判断导出的mongo文件夹大小，来确定是否执行打包命令
 def getdirsize(dir):
     size = 0
@@ -60,15 +74,25 @@ def format_file(format_file_size):
         print "%s   : 当前备份的文件大小是： %sKB\n"%(datetime.datetime.now(),round(size3,1))
     else:#这里对文件大小进行判断，当文件大于1M显示的是多少MB，如果当文件小于1M显示的是多少KB，利用round函数进行四舍五入
         print "%s   : 当前备份的文件大小是： %sB\n"%(datetime.datetime.now(),round(size4,3))
-while True:
-    if mongo_file_size == mongo_file_size:
-        #如果文件大小相同说明导出到文件已经结束。执行打包命令
-        os.system(tar_command)
-        tar_file_size = os.path.getsize(TARGET)
-        format_file(tar_file_size)
-        print '%s   : ........备份成功！！..........\n' %datetime.datetime.now()
-        print '%s   : 备份文件为： %s\n' % (datetime.datetime.now(), TARGET)
-        break
-print
+if retcode==0 and mongo_file_size==mongo_file_size:#条件1：如果导出mongo备份成功，再执行压缩备份，否则不备份；条件2：如果文件大小相同说明导出到文件已经结束。执行打包命令
+    try:
+        retcode1 =subprocess.call(tar_command, shell=True)
+        if retcode1 < 0:
+            print >>sys.stderr, "tar导出子进程被终止，返回码是", -retcode1
+            print '%s   : ........tar备份失败！！..........\n' %datetime.datetime.now()
+        if retcode1 ==0:
+            print '%s   : ........tar备份成功！！..........\n' %datetime.datetime.now()
+            tar_file_size=os.path.getsize(TARGET)
+            format_file(tar_file_size)
+            print '%s   : 备份文件为： %s\n' % (datetime.datetime.now(), TARGET)
+        else:
+            print >>sys.stderr, "tar导出子进程返回码是", retcode1
+            print '%s   : ........tar备份失败！！..........\n' %datetime.datetime.now()
+    except OSError as e:
+        print >>sys.stderr, "tar导出错误信息:", e
+        print '%s   : ........tar备份失败！！..........\n' %datetime.datetime.now()
+time2=datetime.datetime.now()
+time3=time2-time1
+print '此次备份总共耗时:',time3#计算出脚本的执行时长
 print '---------------------------------------------------------------------\n'
 
